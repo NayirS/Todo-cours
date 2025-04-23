a76c9e6cbd7e78c6ba9de198b6c57a8c28ca166e
NayirS <nadow692@gmail.com>
2025-04-23 12:29:19 +0200

    supppression todolist et mondif css de l'index et app.tsx

--- isitech-react/src/App.tsx
+++ isitech-react/src/App.tsx
@@ -1,14 +1,21 @@
-
+import { useState } from "react";
 import Header from "./components/Header";
-import TodoList from "./components/TodoList";
+import Formulaire from "./components/Formulaire";
+import { Todo, todos as initialTodos } from "./components/data"; // Assurez-vous que le chemin est correct
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+  const addTodo = (newTodo: Todo) => {
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div className="min-h-screen bg-gray-100 text-black">
+    <div className="min-h-screen bg-gray-100 p-4 flex flex-col items-center">
       <Header />
-      <main className="max-w-2xl mx-auto p-4">
-        <TodoList />
-      </main>
+      <div className="w-full max-w-4xl mt-4">
+        <Formulaire todos={todos} addTodo={addTodo} />
+      </div>
     </div>
   );
 }
